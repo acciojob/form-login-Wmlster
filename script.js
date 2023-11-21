4f25511980af75cd640a6ce8b8d63f0db00630ae
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-21 19:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
 
+	let fname=document.getElementById("first name");
+	let lname=document.getElementById("last name");
+
+	alert(fname.value);
+	alert(lname.value);
+
 }
